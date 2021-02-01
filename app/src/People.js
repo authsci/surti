884b8b491a38bda3a76176152a0b4ac13d8c9eb5
Greg Anderson <ganderson@1wa.org>
2021-01-31 20:57:36 -0800

    adjust nav menu

--- app/src/People.js
+++ app/src/People.js
@@ -4,8 +4,7 @@ import { Link } from "react-router-dom";
 import Markdown from "markdown-to-jsx";
 import ReactTooltip from "react-tooltip";
 
-const setDate = Date.now()
-
+const setDate = Date.now();
 
 const SPACE_ID = "yzeyubafmmte";
 const ACCESS_TOKEN = "3uqmp9O_VOmdmZhd7VGyTEDbuwrKAyTMLnAfHSZYkdM";
@@ -13,7 +12,9 @@ const contentfulAPI =
 	"https://cdn.contentful.com/spaces/" +
 	SPACE_ID +
 	"/entries?access_token=" +
-	ACCESS_TOKEN + "&" + setDate;
+	ACCESS_TOKEN +
+	"&" +
+	setDate;
 
 export default class People extends React.Component {
 	constructor(props) {
@@ -27,7 +28,6 @@ export default class People extends React.Component {
 			id: this.props.match.params.id,
 			name: this.props.match.params.name,
 		};
-	
 	}
 
 	componentDidMount() {
@@ -38,7 +38,6 @@ export default class People extends React.Component {
 		});
 
 		console.log("People.js");
-
 
 		window.scrollTo(0, 0);
 	}
@@ -66,29 +65,52 @@ export default class People extends React.Component {
 									<b>{people.firstname + " " + people.lastname}</b>
 								</div>
 
-							<div className="desktop">
-								<div className="profile">
-									<div>
-										{people.photo &&
-											media.includes.Asset.map(
-												(image, index) =>
-													people.photo.sys.id ==
-														media.includes.Asset[index].sys.id && (
-														<img
-															key={index}
-															src={media.includes.Asset[index].fields.file.url}
-														/>
-													)
-											)}
+								<div className="desktop">
+									<div className="profile">
+										<div>
+											{people.photo &&
+												media.includes.Asset.map(
+													(image, index) =>
+														people.photo.sys.id ==
+															media.includes.Asset[index].sys.id && (
+															<img
+																key={index}
+																src={
+																	media.includes.Asset[index].fields.file.url
+																}
+															/>
+														)
+												)}
+										</div>
+										<div>
+											<Link
+												to={`/institution/` + people.dept}
+												className={`dept-` + people.dept.toLowerCase()}
+												data-tip
+												data-for="profile"
+											>
+												{people.dept}
+											</Link>
+
+											<h1>{people.firstname + " " + people.lastname}</h1>
+
+											<h2>{people.position}</h2>
+
+											<a href={`mailto:` + people.email} className="link-icon">
+												<i className="far fa-envelope"></i>
+											</a>
+											<a className="link-email" href={`mailto:` + people.email}>
+												{people.email}
+											</a>
+										</div>
 									</div>
-									<div>
-									<Link to={`/institution/`+ people.dept}
-											className={`dept-` + people.dept.toLowerCase()}
-											data-tip
-											data-for="profile"
-										>
+								</div>
+
+								<div className="mobile">
+									<div className="profile">
+										<div className={`dept-` + people.dept.toLowerCase()}>
 											{people.dept}
-										</Link>
+										</div>
 
 										<h1>{people.firstname + " " + people.lastname}</h1>
 
@@ -102,67 +124,34 @@ export default class People extends React.Component {
 										</a>
 									</div>
 								</div>
-							</div>
 
-							<div className="mobile">
-
-								<div className="profile">
-									<div
-										className={`dept-` + people.dept.toLowerCase()}
-									>
-										{people.dept}
-									</div>
-
-									<h1>{people.firstname + " " + people.lastname}</h1>
-
-									<h2>{people.position}</h2>
-
-									<a href={`mailto:` + people.email} className="link-icon">
-										<i className="far fa-envelope"></i>
-									</a>
-									<a className="link-email" href={`mailto:` + people.email}>
-										{people.email}
-									</a>
-
-								<div className="nudge-sm"></div>
-
-
-									{people.photo &&
-										media.includes.Asset.map(
-											(image, index) =>
-												people.photo.sys.id ==
-													media.includes.Asset[index].sys.id && (
-													<img
-														key={index}
-														src={media.includes.Asset[index].fields.file.url}
-													/>
-												)
-										)}
-								</div>
-								</div>
-
-								<div className="nudge-sm"></div>
+								<div className="nudge-md"></div>
 
 								<div className="profile-bio">
-								{people.bio &&
-									<Markdown>{people.bio}</Markdown>
-								}
+									{people.bio && <Markdown>{people.bio}</Markdown>}
 								</div>
 
+								<div className="nudge-md"></div>
+
+								{people.photo &&
+									media.includes.Asset.map(
+										(image, index) =>
+											people.photo.sys.id ==
+												media.includes.Asset[index].sys.id && (
+												<img
+													key={index}
+													src={media.includes.Asset[index].fields.file.url}
+												/>
+											)
+									)}
+
 								<div className="nudge-lg"></div>
-
-
 							</div>
 						</div>
 
-						<ReactTooltip
-											place="top"
-											type="dark"
-											effect="float"
-											id="profile"
-										>
-											{people.institution}
-										</ReactTooltip>
+						<ReactTooltip place="top" type="dark" effect="float" id="profile">
+							{people.institution}
+						</ReactTooltip>
 					</Fragment>
 				)}
 			</Fragment>
