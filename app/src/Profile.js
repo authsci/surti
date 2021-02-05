771ac76093fff82499bdf868d4a287171440a074
Greg Anderson <ganderson@1wa.org>
2021-02-04 21:43:45 -0800

    add emaoi;s

--- app/src/Profile.js
+++ app/src/Profile.js
@@ -24,10 +24,8 @@ export default class Profile extends React.Component {
 		this.state = {
 			loading: true,
 			keyword: "",
-			people: [],
 			media: [],
 			id: this.props.match.params.id,
-			// name: this.props.match.params.name,
 		};
 	}
 
@@ -56,122 +54,31 @@ export default class Profile extends React.Component {
 							(item, index) =>
 								id == item.sys.id && (
 									<div key={index} className="copy">
-										<div className="desktop">
-											<div className="profile">
-												<div>
-													{item.fields.photo &&
-														media.includes.Asset.map(
-															(image, index) =>
-																item.fields.photo.sys.id ==
-																	media.includes.Asset[index].sys.id && (
-																	<img
-																		key={index}
-																		src={
-																			media.includes.Asset[index].fields.file
-																				.url
-																		}
-																	/>
-																)
-														)}
-												</div>
-												<div>
-													{item.fields.inst &&
-														item.fields.inst.map((itemInst, index) => (
-															<Fragment key={index}>
-																{data.map(
-																	(item, index) =>
-																		item.sys.id == itemInst.sys.id &&
-																		item.fields.type == "org" && (
-																			<Fragment key={index}>
-																				<Link
-																					to={
-																						`/institution/` + item.fields.code
-																					}
-																					className={
-																						`dept-` +
-																						item.fields.code.toLowerCase()
-																					}
-																					data-tip
-																					data-for={item.fields.code}
-																				>
-																					{item.fields.code}
-																				</Link>
 
-																				<ReactTooltip
-																					place="top"
-																					type="dark"
-																					effect="float"
-																					id={item.fields.code}
-																				>
-																					{item.fields.name}
-																				</ReactTooltip>
-																			</Fragment>
-																		)
-																)}
-															</Fragment>
-														))}
+<div className="breadcrumbs">
+								<Link to="/home" className="link-breadcrumbs">
+									Home
+								</Link>
+								<span>/</span>
+								<Link to="/list" className="link-breadcrumbs">
+									People
+								</Link>
+								<span>/</span>
+								{data.map(
+										(item, index) =>
+											id == item.sys.id && (
+												<b key={index}>
+													{item.fields.firstname} {item.fields.lastname}
+												</b>
+											)
+									)}
+							</div>
 
-													<h1>
-														{item.fields.firstname + " " + item.fields.lastname}
-													</h1>
-													<h2>{item.fields.position}</h2>
-													<a
-														href={`mailto:` + item.fields.email}
-														className="link-icon"
-													>
-														<i className="far fa-envelope"></i>
-													</a>
-													<a
-														className="link-email"
-														href={`mailto:` + item.fields.email}
-													>
-														{item.fields.email}
-													</a>
-												</div>
-											</div>
-										</div>
-
-										<div className="mobile">
-											<div className="profile">
-												<Link
-													to={`/institution/` + item.fields.dept}
-													className={`dept-` + item.fields.dept.toLowerCase()}
-												>
-													{item.fields.dept}
-												</Link>
-
-												<h1>
-													{item.fields.firstname + " " + item.fields.lastname}
-												</h1>
-
-												<h2>{item.fields.position}</h2>
-
-												<a
-													href={`mailto:` + item.fields.email}
-													className="link-icon"
-												>
-													<i className="far fa-envelope"></i>
-												</a>
-												<a
-													className="link-email"
-													href={`mailto:` + item.fields.email}
-												>
-													{item.fields.email}
-												</a>
-											</div>
-										</div>
-
-										<div className="nudge-md"></div>
-
-										<div className="profile-bio">
-											{item.fields.bio && (
-												<Markdown>{item.fields.bio}</Markdown>
-											)}
-										</div>
-										<div className="nudge-md"></div>
-
-										<div className="mobile">
-											<div className="nudge-md"></div>
+						{data.map(
+							(item, index) =>
+								id == item.sys.id && (
+									<div key={index} className="profile-card-lg">
+										<Link to={`/profile/` + item.sys.id} className="link-inert">
 											{item.fields.photo &&
 												media.includes.Asset.map(
 													(image, index) =>
@@ -185,7 +92,66 @@ export default class Profile extends React.Component {
 															/>
 														)
 												)}
+										</Link>
+
+										<div>
+											{item.fields.inst &&
+												item.fields.inst.map((itemInst, index) => (
+													<Fragment key={index}>
+														{data.map(
+															(item, index) =>
+																item.sys.id == itemInst.sys.id &&
+																item.fields.type == "org" && (
+																	<Fragment key={index}>
+																			<Link
+																				to={`/institution/` + item.fields.code}
+																				className={
+																					`dept-` +
+																					item.fields.code.toLowerCase()
+																				}
+																				data-tip
+																				data-for={item.fields.code}
+																			>
+																				{item.fields.code}
+																			</Link>
+
+																		<ReactTooltip
+																			place="top"
+																			type="dark"
+																			effect="float"
+																			id={item.fields.code}
+																		>
+																			{item.fields.name}
+																		</ReactTooltip>
+																	</Fragment>
+																)
+														)}
+													</Fragment>
+												))}
+											<h1>
+												{item.fields.firstname + " " + item.fields.lastname}
+											</h1>
+											<h2>{item.fields.position}</h2>
+											<a href={`mailto:` + item.fields.email} className="link-icon">
+											<i className="far fa-envelope"></i>
+										</a>
+										<a className="link-email" href={`mailto:` + item.fields.email}>
+											{item.fields.email}
+										</a>
+									
 										</div>
+									</div>
+								)
+						)}
+
+										<div className="nudge-md"></div>
+
+										<div className="profile-bio">
+											{item.fields.bio && (
+												<Markdown>{item.fields.bio}</Markdown>
+											)}
+										</div>
+								
 
 										<div className="nudge-md"></div>
 
