a352f2d4edad377ef5b27a451943828b7374469e
Greg Anderson <ganderson@1wa.org>
2021-06-22 13:46:41 -0700

    fixed events link

--- app/src/EventCard.js
+++ app/src/EventCard.js
@@ -49,45 +49,83 @@ export default class ProfileCard extends React.Component {
 			<Fragment>
 
 			
-					<div className="profile-container">
 
 						{data.map(
 							(item, index) =>
 								id == item.sys.id && (
-									<div key={index} className="profile-card">
-										<Link to={`/event/` + item.sys.id} className="link-inert">
-											{item.fields.photo &&
-												media.includes.Asset.map(
-													(image, index) =>
-														item.fields.photo.sys.id ==
-															media.includes.Asset[index].sys.id && (
-															<img
+									<div key={index} className="event-container">
+												<div className="event-card">
+													<Link
+														to={
+															"/event/" + item.sys.id + "/" + item.fields.title
+														}
+														className="link-inert"
+													>
+														{item.fields.graphic ? (
+															media.includes.Asset.map(
+																(image, index) =>
+																	item.fields.graphic.sys.id ==
+																		media.includes.Asset[index].sys.id && (
+																		<div
+																			key={index}
+																			style={{
+																				backgroundImage:
+																					"url(" +
+																					media.includes.Asset[index].fields
+																						.file.url +
+																					")",
+																				backgroundSize: "cover",
+																				backgroundPosition: "center center",
+																				height: "175px",
+																				width: "100%",
+																				backgroundRepeat: "no-repeat",
+																			}}
+																		></div>
+																	)
+															)
+														) : (
+															<div
 																key={index}
-																src={
-																	media.includes.Asset[index].fields.file.url
-																}
-															/>
-														)
-												)}
-										</Link>
+																style={{
+																	backgroundImage: 'url("img/placeholder.png")',
+																	backgroundSize: "cover",
+																	backgroundPosition: "center center",
+																	height: "175px",
+																	width: "100%",
+																}}
+															></div>
+														)}
+													</Link>
 
-										<div>
-											
-											<h1>
-												{item.fields.title}
-											</h1>
-											<h2>{item.fields.position}</h2>
-											<Link
-												to={`/event/` + item.sys.id}
-												className="link-default"
-											>
-												View Event Details
-											</Link>
-										</div>
-									</div>
+													<div>
+														<h1>{item.fields.title}</h1>
+														<h2>{item.fields.subtext}</h2>
+														{item.fields.year && <h3>{item.fields.year}
+													<div className="nudge-sm"></div>
+													</h3> }
+
+
+													{ item.fields.showEventDate && <p><Moment
+																format="LL"
+																date={item.fields.eventDate}
+															/></p>
+													}
+														<Link
+															to={
+																"/event/" +
+																item.sys.id +
+																"/" +
+																item.fields.title
+															}
+															className="link-default"
+														>
+															View Event Details
+														</Link>
+													</div>
+												</div>
+											</div>
 								)
 						)}
-					</div>
 			</Fragment>
 		);
 	}
